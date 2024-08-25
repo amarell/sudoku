a67ac37efb0d798023b6d4039874e737728ecb98
amarell <mujagicamar@gmail.com>
2024-08-25 16:42:39 +0200

    Cleanup and added more unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use core::fmt;
-use std::{fmt::Display, result};
+use std::result;
 
 type Result<T> = result::Result<T, ()>;
 
@@ -11,15 +10,15 @@ fn printing(g: &Vec<Vec<i32>>) {
 
 fn main() {
     let input = vec![
-        vec![3, 0, 6, 5, 0, 8, 4, 0, 0],
-        vec![5, 2, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 8, 7, 0, 0, 0, 0, 3, 1],
-        vec![0, 0, 3, 0, 1, 0, 0, 8, 0],
-        vec![9, 0, 0, 8, 6, 3, 0, 0, 5],
-        vec![0, 5, 0, 0, 9, 0, 6, 0, 0],
-        vec![1, 3, 0, 0, 0, 0, 2, 5, 0],
-        vec![0, 0, 0, 0, 0, 0, 0, 7, 4],
-        vec![0, 0, 5, 2, 0, 6, 3, 0, 0],
+        vec![0, 0, 0, 0, 0, 2, 0, 0, 0],
+        vec![7, 3, 0, 0, 5, 0, 1, 0, 0],
+        vec![0, 1, 0, 0, 0, 0, 5, 3, 0],
+        vec![5, 0, 0, 0, 4, 0, 0, 0, 0],
+        vec![3, 4, 2, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 8, 6, 0, 0, 5, 0],
+        vec![9, 0, 0, 0, 0, 1, 0, 0, 0],
+        vec![0, 0, 0, 4, 3, 0, 0, 0, 6],
+        vec![0, 0, 0, 0, 0, 0, 8, 0, 0],
     ];
 
     if let Ok(solution) = solve_naive(&input) {
@@ -34,31 +33,31 @@ fn main() {
 fn solve_naive(input: &Vec<Vec<i32>>) -> Result<Vec<Vec<i32>>> {
     let mut grid = input.clone();
 
-    if f(&mut grid, 0, 0) {
+    if solve_sudoku_naive(&mut grid, 0, 0) {
         Ok(grid)
     } else {
         Err(())
     }
 }
 
-fn f(g: &mut Vec<Vec<i32>>, r: usize, c: usize) -> bool {
+fn solve_sudoku_naive(g: &mut Vec<Vec<i32>>, r: usize, c: usize) -> bool {
     if (r == 8) && (c == 9) {
         return true;
     }
 
     if c == 9 {
-        return f(g, r + 1, 0);
+        return solve_sudoku_naive(g, r + 1, 0);
     }
 
     if g[r][c] != 0 {
-        return f(g, r, c + 1);
+        return solve_sudoku_naive(g, r, c + 1);
     }
 
     // The spot is not filled, attempt all possiblities
     for i in 1..=9 {
         if is_safe(g, r, c, i) {
             g[r][c] = i;
-            if f(g, r, c + 1) {
+            if solve_sudoku_naive(g, r, c + 1) {
                 return true;
             }
 
@@ -108,6 +107,7 @@ fn is_safe(g: &Vec<Vec<i32>>, r: usize, c: usize, num: i32) -> bool {
     true
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
 
@@ -138,5 +138,52 @@ mod tests {
         ];
 
         assert_eq!(expected, solve_naive(&input).unwrap())
+    }
+
+    #[test]
+    fn test_naive_expert_difficulty() {
+        let input = vec![
+            vec![0, 0, 0, 0, 0, 2, 0, 0, 0],
+            vec![7, 3, 0, 0, 5, 0, 1, 0, 0],
+            vec![0, 1, 0, 0, 0, 0, 5, 3, 0],
+            vec![5, 0, 0, 0, 4, 0, 0, 0, 0],
+            vec![3, 4, 2, 0, 0, 0, 0, 0, 0],
+            vec![0, 0, 0, 8, 6, 0, 0, 5, 0],
+            vec![9, 0, 0, 0, 0, 1, 0, 0, 0],
+            vec![0, 0, 0, 4, 3, 0, 0, 0, 6],
+            vec![0, 0, 0, 0, 0, 0, 8, 0, 0],
+        ];
+
+        let expected = vec![
+            vec![6, 8, 5, 3, 1, 2, 4, 7, 9],
+            vec![7, 3, 4, 9, 5, 8, 1, 6, 2],
+            vec![2, 1, 9, 6, 7, 4, 5, 3, 8],
+            vec![5, 6, 8, 2, 4, 7, 9, 1, 3],
+            vec![3, 4, 2, 1, 9, 5, 6, 8, 7],
+            vec![1, 9, 7, 8, 6, 3, 2, 5, 4],
+            vec![9, 2, 6, 7, 8, 1, 3, 4, 5],
+            vec![8, 5, 1, 4, 3, 9, 7, 2, 6],
+            vec![4, 7, 3, 5, 2, 6, 8, 9, 1],
+        ];
+
+        assert_eq!(expected, solve_naive(&input).unwrap())
+    }
+
+    #[test]
+    fn test_no_solution() {
+        let input = vec![
+            //   v - This column has two 7s
+            vec![7, 0, 0, 0, 0, 0, 0, 0, 0],
+            vec![7, 3, 0, 0, 5, 0, 1, 0, 0],
+            vec![0, 1, 0, 0, 0, 0, 5, 3, 0],
+            vec![5, 0, 0, 0, 4, 0, 0, 0, 0],
+            vec![3, 4, 2, 0, 0, 0, 0, 0, 0],
+            vec![0, 0, 0, 8, 6, 0, 0, 5, 0],
+            vec![9, 0, 0, 0, 0, 1, 0, 0, 0],
+            vec![0, 0, 0, 4, 3, 0, 0, 0, 6],
+            vec![0, 0, 0, 0, 0, 0, 8, 0, 0],
+        ];
+
+        assert_eq!(Err(()), solve_naive(&input))
     }
 }
